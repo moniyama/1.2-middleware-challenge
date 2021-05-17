92ecd071f2901fe417cbd5d1d1210fe2e90287f6
Monica <monicasayama@gmail.com>
2021-05-17 12:05:44 -0300

    add exist user account  function

--- src/index.js
+++ src/index.js
@@ -10,11 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ message: "User não existe" })
+  }
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
 }
 
 function checksTodoExists(request, response, next) {

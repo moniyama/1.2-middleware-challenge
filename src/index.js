deb331083acb06546522c19b18ab05b5bab7fad1
Monica <monicasayama@gmail.com>
2021-05-17 12:10:23 -0300

    add user availability funciont

--- src/index.js
+++ src/index.js
@@ -21,6 +21,11 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
+  const { user } = request
+  if(user.pro || user.todos.length < 10) {
+    return next()
+  }
+  return response.status(403)
 }
 
 function checksTodoExists(request, response, next) {
